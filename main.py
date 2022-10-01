924304788384852625bdbf1d88632fa5a200a119
toma-sh <zoltom83@gmail.com>
2022-10-02 00:34:53 +0200

    kolejna funkcja

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+MAX_LINES = 3
+
 
 def deposit():
     while True:
-        amount = input("What would you like to deposit? $")
+        amount = input("What would you like to deposit? $ ")
         if amount.isdigit():
             amount = int(amount)
             if amount>0:
@@ -10,4 +12,27 @@ def deposit():
                 print("Amount must be greater than 0.")
         else:
             print("Please enter a number.")
-            
+
+    return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number od lines to bet on (1-" +str(MAX_LINES)+ ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1<=lines<=MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    print(balance,lines)
+
+
+main()
