d4753fdc26c11e91f2ea14552bbfebd960656c72
toma-sh <zoltom83@gmail.com>
2022-10-05 21:33:19 +0200

    add get_slot_machine_spin()

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
+import random
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
+
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+def get_slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+    
+    columns=[]
+    for _ in range(cols):
+        column=[]
+        current_symbol=all_symbols[:]
+        for _ in range(rows):
+            value=random.choice(current_symbol)
+            current_symbol.remove(value)
+            column.append(value)
+
+        columns.append(column)
 
 
 def deposit():
@@ -34,7 +64,7 @@ def get_number_of_lines():
 
 def get_bet():
     while True:
-        amount = input("What would you like to bet? $ ")
+        amount = input("What would you like to bet on each line? $ ")
         if amount.isdigit():
             amount = int(amount)
             if MIN_BET <= amount <= MAX_BET:
@@ -50,7 +80,17 @@ def get_bet():
 def main():
     balance = deposit()
     lines = get_number_of_lines()
-    print(balance,lines)
+
+    while True:
+        bet = get_bet()
+        total_bet = bet*lines
+
+        if total_bet > balance:
+            print(f"You do not have enough to bet that amount, your current balance is: ${balance}")
+        else:
+            break
+
+    print(f"You are betting ${bet} on {lines}. Total bet  is equal to: ${total_bet}")
 
 
 main()
